79642dad2ce084701eacd51eaa49452e80603f98
Ali Nasrabadi <nasrabadiam@cafebazaar.ir>
2022-08-10 16:50:04 +0430

    Make project fail

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,23 +1,15 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.chuckerteam.chucker.api.ChuckerInterceptor
-import dev.kiki.core.UpdateManagerFactory
 
-class MainActivity:AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        ChuckerInterceptor.Builder(this).build()
-        val updateManager = UpdateManagerFactory.create(this)
-        findViewById<Button>(R.id.button).setOnClickListener {
-            Toast.makeText(this,"Heelo",Toast.LENGTH_LONG).show()
-            updateManager.startUpdateFlow(this)
-        }
-
+        val chucker = ChuckerInterceptor.Builder(this).build()
+        chucker.redactHeader("name")
     }
 }
